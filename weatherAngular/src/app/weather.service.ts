7798ee9ea67877d6a9597799d39b4fe40a16fe14
Zhdzd <zahidabdullah18@gmail.com>
2022-02-09 13:38:46 +0800

    'completed'

--- weatherAngular/src/app/weather.service.ts
+++ weatherAngular/src/app/weather.service.ts
@@ -10,7 +10,8 @@ export class WeatherService{
 
   getWeather(city: string): Promise<Weather>{
     return lastValueFrom(
-      this.http.get<Weather>(`http://localhost:8080/api/weather/${city}`)
+      //this.http.get<Weather>(`http://localhost:8080/api/weather/${city}`)
+      this.http.get<Weather>(`/api/weather/${city}`)
     )
   }
 
